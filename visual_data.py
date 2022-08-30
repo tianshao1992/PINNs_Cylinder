d1f317092f3918af6f1dcd4b29334f6a717890e2
yunbattle1994 <58914317+yunbattle1994@users.noreply.github.com>
2022-08-31 01:21:30 +0800

    v 1.1 change visual

--- visual_data.py
+++ visual_data.py
@@ -8,6 +8,8 @@ from scipy import stats
 from matplotlib.animation import FuncAnimation
 import matplotlib.tri as tri
 import matplotlib.cm as cm
+from matplotlib import ticker, rcParams
+from matplotlib.ticker import MultipleLocator
 
 
 class matplotlib_vision(object):
@@ -28,11 +30,11 @@ class matplotlib_vision(object):
         self.fig, self.axes = plt.subplots(len(self.field_name), 3, gridspec_kw=gs_dict, num=100, figsize=(30, 20))
         self.font = {'family': 'Times New Roman', 'weight': 'normal', 'size': 30}
 
-    def plot_loss(self, x, y, label, title=None):
+    def plot_loss(self, x, y, label, title=None, color=None):
         # sbn.set_style('ticks')
         # sbn.set(color_codes=True)
 
-        plt.plot(x, y, label=label)
+        plt.plot(x, y, label=label, color=color)
         plt.semilogy()
         plt.grid(True)  # 添加网格
         plt.legend(loc="upper right", prop=self.font)
@@ -167,8 +169,9 @@ class matplotlib_vision(object):
 
             ########      Exact f(t,x,y)     ###########
             plt.subplot(3, Num_fields, 0 * Num_fields + fi + 1)
+            # plt.axis('equal')
             f_true = out_true[:, :, fi]
-            plt.pcolormesh(x_pos, y_pos, f_true, cmap='jet', shading='gouraud', antialiased=True, snap=True)
+            plt.pcolormesh(x_pos, y_pos, f_true, cmap='RdYlBu_r', shading='gouraud', antialiased=True, snap=True)
 
             # plt.contourf(x_pos, y_pos, f_true, cmap='jet',)
             if cmin_max is not None:
@@ -176,52 +179,62 @@ class matplotlib_vision(object):
             plt.clim(vmin=fmin[fi], vmax=fmax[fi])
             cb = plt.colorbar()
             cb.ax.tick_params(labelsize=20)  # 设置色标刻度字体大小
+            tick_locator = ticker.MaxNLocator(nbins=3)  # colorbar上的刻度值个数
+            cb.locator = tick_locator
+            cb.update_ticks()
             plt.rcParams['font.family'] = 'Times New Roman'
             # cb.set_label('value', rotation=0, fontdict=self.font, y=1.08)
             plt.rcParams['font.size'] = 20
             # plt.xlabel('$x$', fontdict=self.font)
-            plt.ylabel('$' + self.input_name[1] + '$', fontdict=self.font)
-            plt.title('True field $' + field_name[fi] + '$' + '', fontsize=30)
+            # plt.ylabel('$' + self.input_name[1] + '$', fontdict=self.font)
+            plt.title('Original field $' + field_name[fi] + '$' + '', fontdict=self.font)
 
 
             ########     Learned f(t,x,y)     ###########
             plt.subplot(3, Num_fields, 1 * Num_fields + fi + 1)
+            # plt.axis('equal')
             f_pred = out_pred[:, :, fi]
-            plt.pcolormesh(x_pos, y_pos, f_pred, cmap='jet', shading='gouraud', antialiased=True, snap=True)
+            plt.pcolormesh(x_pos, y_pos, f_pred, cmap='RdYlBu_r', shading='gouraud', antialiased=True, snap=True)
             # plt.contourf(x_pos, y_pos, f_pred, cmap='jet',)
             cb = plt.colorbar()
             if cmin_max is not None:
                 plt.axis((cmin[0], cmax[0], cmin[1], cmax[1]))
             plt.clim(vmin=fmin[fi], vmax=fmax[fi])
             cb.ax.tick_params(labelsize=20)  # 设置色标刻度字体大小
+            tick_locator = ticker.MaxNLocator(nbins=3)  # colorbar上的刻度值个数
+            cb.locator = tick_locator
+            cb.update_ticks()
             plt.rcParams['font.size'] = 20
             # plt.xlabel('$x$', fontdict=self.font)
-            plt.ylabel('$' + self.input_name[1] + '$', fontdict=self.font)
-            plt.title('Pred field $' + field_name[fi] + '$' + '', fontsize=30)
+            # plt.ylabel('$' + self.input_name[1] + '$', fontdict=self.font)
+            plt.title('Predicted field $' + field_name[fi] + '$' + '', fontdict=self.font)
 
             ########     Error f(t,x,y)     ###########
             plt.subplot(3, Num_fields, 2 * Num_fields + fi + 1)
+            # plt.axis('equal')
             err = f_true - f_pred
             plt.pcolormesh(x_pos, y_pos, err, cmap='coolwarm', shading='gouraud', antialiased=True, snap=True)
             # plt.contourf(x_pos, y_pos, err, cmap='coolwarm', )
             if cmin_max is not None:
                 plt.axis((cmin[0], cmax[0], cmin[1], cmax[1]))
             cb = plt.colorbar()
-            plt.clim(vmin=-max(abs(fmin[fi]), abs(fmax[fi])), vmax=max(abs(fmin[fi]), abs(fmax[fi])))
+            err_bar = np.abs(err).max()
+            plt.clim(vmin=-err_bar, vmax=err_bar)
+
             cb.ax.tick_params(labelsize=20)  # 设置色标刻度字体大小
             plt.rcParams['font.size'] = 20
-            plt.xlabel('$' + self.input_name[0] + '$', fontdict=self.font)
-            plt.ylabel('$' + self.input_name[1] + '$', fontdict=self.font)
-            plt.title('field error$' + field_name[fi] + '$' + '', fontsize=30)
+            # plt.xlabel('$' + self.input_name[0] + '$', fontdict=self.font)
+            # plt.ylabel('$' + self.input_name[1] + '$', fontdict=self.font)
+            plt.title('field error $\it{' + field_name[fi] + '}$' + '', fontdict=self.font)
 
-    def plot_fields_am(self, out_true, out_pred, coord, p_id, fig):
+    def plot_fields_am(self, out_true, out_pred, coord, p_id, fig, cmin_max=None):
 
         fmax = out_true.max(axis=(0, 1, 2))  # 云图标尺
         fmin = out_true.min(axis=(0, 1, 2))  # 云图标尺
 
         def anim_update(t_id):
             print('para:   ' + str(p_id) + ',   time:   ' + str(t_id))
-            axes = self.plot_fields_ms(out_true[t_id], out_pred[t_id], coord[t_id], fmin_max=(fmin, fmax))
+            axes = self.plot_fields_ms(out_true[t_id], out_pred[t_id], coord[t_id], cmin_max=cmin_max, fmin_max=(fmin, fmax))
             return axes
 
         anim = FuncAnimation(fig, anim_update,
