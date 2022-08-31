3d35c926f7b3ded34f2017c872b69ecda6f6768d
lgumd <leigao0910@gmail.com>
2022-08-30 21:47:38 -0400

    convert to paddle version

---
+++ basic_model_pdpd.py
@@ -0,0 +1,194 @@
+import paddle
+import paddle.nn as nn
+import numpy as np
+
+def gradients(y, x, order=1, create=True):
+    if order == 1:
+        return paddle.grad(y, x, grad_outputs=paddle.ones_like(y),
+                                   create_graph=create, retain_graph=True, only_inputs=True)[0]
+    else:
+        return paddle.stack([paddle.grad([y[:, i].sum()], [x], create_graph=True, retain_graph=True)[0]
+                            for i in range(y.shape[1])], axis=-1)
+
+
+def index_tensor(x, idx):
+    return paddle.to_tensor(x.numpy().inx)
+
+def jacobians(u, x, order=1):
+    if order == 1:
+        return paddle.autograd.functional.jacobian(u, x, create_graph=True)[0]
+
+
+def initialize_weights(net):
+    for m in net.sublayers():
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_normal_(m.weight, gain=1)
+            # nn.init.xavier_uniform(m.weight, gain=1)
+            m.bias.data.zero_()
+
+class DeepModel_multi(nn.Layer):
+    def __init__(self, planes, data_norm, active=nn.GELU()):
+        super(DeepModel_multi, self).__init__()
+        self.planes = planes
+        self.active = active
+
+        self.x_norm = data_norm[0]
+        self.f_norm = data_norm[1]
+        self.g_norm = data_norm[2]
+        self.layers = nn.ModuleList()
+        for j in range(self.planes[-1]):
+            layer = []
+            for i in range(len(self.planes) - 2):
+                layer.append(nn.Linear(self.planes[i], self.planes[i + 1]))
+                layer.append(self.active)
+            layer.append(nn.Linear(self.planes[-2], 1))
+            self.layers.append(nn.Sequential(*layer))
+        self.apply(initialize_weights)
+
+    def forward(self, in_var):
+        in_var = self.x_norm.norm(in_var)
+        y = []
+        for i in range(self.planes[-1]):
+            y.append(self.layers[i](in_var))
+        return self.f_norm.back(paddle.concat(y, axis=-1))
+
+    def loadmodel(self, File):
+        try:
+            checkpoint = paddle.load(File)
+            self.set_state_dict(checkpoint['model'])        # 从字典中依次读取
+            start_epoch = checkpoint['epoch']
+            print("load start epoch at " + str(start_epoch))
+            try:
+                log_loss = checkpoint['log_loss']
+            except:
+                log_loss= []
+            return start_epoch, log_loss
+        except:
+            print("load model failed, start a new model!")
+            return 0, []
+
+    def equation(self, inv_var, out_var):
+        return 0
+
+class DeepModel_single(nn.Layer):
+    def __init__(self, planes, data_norm, active=nn.GELU()):
+        super(DeepModel_single, self).__init__()
+        self.planes = planes
+        self.active = active
+
+        self.x_norm = data_norm[0]
+        self.f_norm = data_norm[1]
+        if len(data_norm) > 2:
+            self.g_norm = data_norm[2]
+        self.layers = nn.LayerList()
+        for i in range(len(self.planes)-2):
+            self.layers.append(nn.Linear(self.planes[i], self.planes[i + 1]))
+            self.layers.append(self.active)
+        self.layers.append(nn.Linear(self.planes[-2], self.planes[-1]))
+
+        self.layers = nn.Sequential(*self.layers)
+        # self.apply(initialize_weights)
+
+    def forward(self, inn_var, is_norm=True):
+        inn_var = self.x_norm.norm(inn_var)
+        out_var = self.layers(inn_var)
+
+        if is_norm:
+            return self.f_norm.back(out_var)
+        else:
+            return out_var
+
+
+    def loadmodel(self, File):
+        try:
+            checkpoint = paddle.load(File)
+            self.set_state_dict(checkpoint['model'])        # 从字典中依次读取
+            start_epoch = checkpoint['epoch']
+            print("load start epoch at " + str(start_epoch))
+            log_loss = checkpoint['log_loss']
+            return start_epoch, log_loss
+        except:
+            print("load model failed, start a new model!")
+            return 0, []
+
+    def equation(self, **kwargs):
+        return 0
+
+    def dirichlet(self, ind, out_var, value, is_norm=False):
+        if is_norm:
+            res = self.f_norm.norm(out_var[ind, :]) - self.f_norm.norm(value[ind].unsqueeze(-1))
+        else:
+            res = out_var[ind, :] - value[ind, :]
+        return res
+
+    def neumann(self, ind, out_var, in_var, value, norm):
+        duda = gradients(out_var.sum(), in_var)
+        dudx = duda[ind, :1]
+        dudy = duda[ind, 1:]
+        res = norm[ind, :1] * dudx + norm[ind, 1:] * dudy - value[ind, :]
+        return res
+
+
+class Dynamicor(nn.Layer):
+
+    def __init__(self, device, coords):
+        super(Dynamicor, self).__init__()
+        self.c, self.rho, self.uinf = 1.0, 1.0, 1.0
+        self.Re = 250
+        self.miu = self.c * self.rho * self.uinf / self.Re
+        self.device = device
+        self.coords = paddle.to_tensor(coords, dtype='float32', place=device)
+
+        cir_num = self.coords.shape[0]
+        self.ind1 = [i for i in range(cir_num)]
+        self.ind2 = [i for i in range(1, cir_num)]
+        self.ind2.append(0)
+
+        self.T_vector = paddle.to_tensor(self.coords.numpy()[self.ind2, 0, :]) - paddle.to_tensor(self.coords.numpy()[self.ind1, 0, :])
+
+        self.N_vector = paddle.matmul(self.T_vector, paddle.to_tensor(np.array([[0, -1], [1, 0]]),
+                                                                      dtype='float32', place=self.device))
+        self.T_norm = paddle.norm(self.T_vector, axis=-1)
+        self.N_norm = paddle.norm(self.N_vector, axis=-1)
+        self.delta = self.coords[:, 0] - self.coords[:, 1]
+        self.delta = paddle.norm(self.delta, axis=-1)
+
+
+
+    def cal_force(self, fields):
+
+        p =fields[:, :, 0, 0]
+        p_ave = paddle.to_tensor(p.numpy()[:, self.ind1]) + paddle.to_tensor(p.numpy()[:, self.ind2]) / 2.
+        Ft_n = p_ave * self.T_norm
+        Fx = - Ft_n * self.N_vector[:, 0] / self.N_norm
+        Fy = - Ft_n * self.N_vector[:, 1] / self.N_norm
+
+        # tao = -miu * du/dn at grid point
+        # fields : [96, 198, (p,u,v)]
+
+        u = fields[:, :, 1, 1:]
+        du = (u[:, :, 0] * self.T_vector[:, 0] + u[:, :, 1] * self.T_vector[:, 1]) / self.T_norm
+        tau = du / self.delta * self.miu
+        tau_ave = (paddle.to_tensor(tau.numpy()[:, self.ind1]) + paddle.to_tensor(tau.numpy()[:, self.ind2])) * 0.5
+        # tau_ave = (tau[:, self.ind1] + tau[:, self.ind2]) * 0.5
+        T_n = tau_ave * self.T_norm
+        Tx = T_n * self.T_vector[:, 0] / self.T_norm
+        Ty = T_n * self.T_vector[:, 1] / self.T_norm
+
+        Fx = paddle.sum(Fx, axis=1)
+        Fy = paddle.sum(Fy, axis=1)
+        Tx = paddle.sum(Tx, axis=1)
+        Ty = paddle.sum(Ty, axis=1)
+
+        return Fx, Fy, Tx, Ty
+
+
+    def forward(self, fields):
+
+        Fx, Fy, Tx, Ty = self.cal_force(fields)
+
+        Fx += Tx
+        Fy += Ty
+        CL = Fy/(0.5*self.rho*self.uinf**2)
+        CD = Fx / (0.5 * self.rho * self.uinf ** 2)
+        return paddle.stack((Fy, Fx, CL, CD), axis=-1)
---
+++ run_validate_pdpd.py
@@ -0,0 +1,184 @@
+import numpy as np
+import paddle
+from process_data_pdpd import data_norm
+from basic_model_pdpd import Dynamicor
+import visual_data
+import matplotlib.pyplot as plt
+import os
+import argparse
+from run_train_pdpd import read_data, Net, inference
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+def get_args():
+
+    parser = argparse.ArgumentParser('PINNs for naiver-stokes cylinder with Karman Vortex', add_help=False)
+    parser.add_argument('--points_name', default="pdpd_24+6+4", type=str)
+    parser.add_argument('--Nx_EQs', default=30000, type=int, help="xy sampling in for equation loss")
+    parser.add_argument('--Nt_EQs', default=15, type=int, help="time sampling in for equation loss")
+    parser.add_argument('--Nt_BCs', default=120, type=int, help="time sampling in for boundary loss")
+    parser.add_argument('--epochs_adam', default=400000, type=int)
+    parser.add_argument('--save_freq', default=2000, type=int, help="frequency to save model and image")
+    parser.add_argument('--print_freq', default=500, type=int, help="frequency to print loss")
+    parser.add_argument('--device', default=0, type=int, help="time sampling in for boundary loss")
+    parser.add_argument('--data_path', default='./data/cyl_Re250.mat', type=str, help="data path for cylinder")
+
+    return parser.parse_args()
+
+# def cal_force():
+
+
+if __name__ == '__main__':
+
+    opts = get_args()
+    print(opts)
+    np.random.seed(2022)
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(opts.device)  # 指定第一块gpu
+
+    if paddle.fluid.is_compiled_with_cuda():
+        paddle.set_device("gpu")
+        device = 'gpu'
+    else:
+        paddle.set_device('cpu')
+        device = 'cpu'
+
+    points_name = opts.points_name
+    work_name = 'NS-cylinder-2d-t_' + points_name
+    work_path = os.path.join('work', work_name, )
+    vald_path = os.path.join('work', work_name, 'validation')
+    isCreated = os.path.exists(vald_path)
+    if not isCreated:
+        os.makedirs(vald_path)
+
+    times, nodes, field = read_data()
+    Dyn_model = Dynamicor(device, nodes[:, (0,1), :])
+
+    Nt, Nx, Ny, Nf = field.shape[0], field.shape[1], field.shape[2], field.shape[3]
+
+    times = np.tile(times[:, None, None, :], (1, Nx, Ny, 1))
+    nodes = np.tile(nodes[None, :, :, :], (Nt, 1, 1, 1))
+    times = times.reshape(-1, 1)
+    nodes = nodes.reshape(-1, 2)
+    # field = field.reshape(-1, Nf)
+    input = np.concatenate((nodes, times), axis=-1)
+    input_norm = data_norm(input, method='mean-std')
+    field_norm = data_norm(field, method='mean-std')
+
+    input_visual = input.reshape((Nt, Nx, Ny, 3))
+    add_input = input_visual[:, 0, :, :].reshape((Nt, -1, Ny, 3))
+    input_visual = np.concatenate((input_visual, add_input), axis=1)
+    field_visual = field.reshape((Nt, Nx, Ny, Nf))
+    add_field = field_visual[:, 0, :, :].reshape((Nt, -1, Ny, Nf))
+    field_visual = np.concatenate((field_visual, add_field), axis=1)
+
+    Net_model = Net(planes=[3, 64, 64, 64, 64, 64, 64, 3], data_norm=(input_norm, field_norm)).to(device)
+    Visual = visual_data.matplotlib_vision(vald_path, field_name=('p', 'u', 'v'), input_name=('x', 'y'))
+    Visual.font['size'] = 20
+    start_epoch, log_loss = Net_model.loadmodel(os.path.join(work_path, 'latest_model.pth'))
+
+####################################### plot loss #################################################################################
+    print("plot training loss")
+    plt.figure(1, figsize=(15, 10))
+    plt.clf()
+    Visual.plot_loss(np.arange(len(log_loss)), np.array(log_loss)[:, -1], 'unsupervised loss')
+    Visual.plot_loss(np.arange(len(log_loss)), np.array(log_loss)[:, 0], 'conversation loss')
+    plt.savefig(os.path.join(vald_path, 'loss_eqs_data.jpg'), dpi=300)
+
+    plt.figure(2, figsize=(15, 10))
+    plt.clf()
+    Visual.plot_loss(np.arange(len(log_loss)), np.array(log_loss)[:, 1], 'inlet boundary loss')
+    Visual.plot_loss(np.arange(len(log_loss)), np.array(log_loss)[:, 2], 'outlet boundary loss')
+    Visual.plot_loss(np.arange(len(log_loss)), np.array(log_loss)[:, 3], 'cylinder boundary loss')
+    Visual.plot_loss(np.arange(len(log_loss)), np.array(log_loss)[:, 4], 'measurement loss')
+    Visual.plot_loss(np.arange(len(log_loss)), np.array(log_loss)[:, 5], 'initial boundary loss')
+    plt.savefig(os.path.join(vald_path, 'loss_boundary.jpg'), dpi=300)
+
+####################################### plot several fields #################################################################################
+    print("plot several true and predicted fields")
+    inds = np.concatenate((np.zeros((1,), dtype=np.int32), np.linspace(0, 100, 11, dtype=np.int32)))
+    input_visual_p = paddle.to_tensor(input_visual[inds], dtype='float32', place=device)
+    field_visual_p = inference(input_visual_p, Net_model)
+    field_visual_t = field_visual[inds]
+    field_visual_p = field_visual_p.cpu().numpy()
+    input_visual_p = input_visual_p.cpu().numpy()
+    ori_input = input_norm.back(input_visual_p[:, 0, 0, :])
+    for t in range(len(inds)):
+        plt.figure(3, figsize=(20, 10))
+        plt.clf()
+        Visual.plot_fields_ms(field_visual_t[t], field_visual_p[t], input_visual_p[0, :, :, :2],
+                              cmin_max=[[-4, -4], [10, 4]], field_name=['p', 'u', 'v'])
+        # plt.suptitle('$t$ = ' + str(ori_input[t, 0]) + ' T', )
+        plt.subplots_adjust(wspace=0.2, hspace=0.3)#left=0.05, bottom=0.05, right=0.95, top=0.95
+        plt.savefig(os.path.join(vald_path, 'loca_' + str(inds[t]) + '.jpg'))
+
+        plt.figure(4, figsize=(15, 12))
+        plt.clf()
+        Visual.plot_fields_ms(field_visual_t[t], field_visual_p[t], input_visual_p[0, :, :, :2])
+        plt.subplots_adjust(wspace=0.2, hspace=0.3)
+        plt.savefig(os.path.join(vald_path, 'full_' + str(inds[t]) + '.jpg'))
+
+####################################### plot continous fields #################################################################################
+
+    input_visual_p = paddle.to_tensor(input_visual[::5], dtype='float32', place=device)
+    field_visual_p = inference(input_visual_p, Net_model)
+    field_visual_t = field_visual[::5]
+    field_visual_p = field_visual_p.cpu().numpy()
+    input_visual_p = input_visual_p.cpu().numpy()
+    ori_input = input_norm.back(input_visual_p[:, 0, 0, :])
+
+    # plot continous fields at points
+    ind_xs = np.random.choice(np.arange(0, Nx), 4)
+    ind_ys = np.random.choice(np.arange(0, Ny), 4)
+    plt.figure(6, figsize=(15, 10))
+    plt.clf()
+    lims = [[-1, 1], [-0.5, 1.5], [-1, 1]]
+    tits = ['p', 'u', 'v']
+    for j in range(3):
+        plt.subplot(2, 2, j+1)
+        plt.ylim(lims[j])
+        plt.title(tits[j])
+        for i, (ind_x, ind_y) in enumerate(zip(ind_xs, ind_ys)):
+            plt.plot(ori_input[:, -1], field_visual_t[:, ind_x, ind_y, j])
+            plt.scatter(ori_input[:, -1], field_visual_p[:, ind_x, ind_y, j])
+        plt.legend(['original', 'predicted'])
+    plt.savefig(os.path.join(vald_path, 'contous_fields_point.jpg'))
+
+    ####################################### plot dynamic coefficient ############################################################################
+    print("plot dynamic coefficient")
+    ori_forces = Dyn_model(paddle.to_tensor(field_visual_t[:, :-1], place=device))
+    pre_forces = Dyn_model(paddle.to_tensor(field_visual_p[:, :-1], place=device))
+    ori_forces = ori_forces.cpu().numpy()
+    pre_forces = pre_forces.cpu().numpy()
+
+    plt.figure(6, figsize=(15, 10))
+    plt.clf()
+    plt.plot(ori_input[:, -1], ori_forces[:, 0], 'r-')
+    plt.plot(ori_input[:, -1], pre_forces[:, 0], 'ro')
+    plt.plot(ori_input[:, -1], ori_forces[:, 1], 'b-')
+    plt.plot(ori_input[:, -1], pre_forces[:, 1], 'bo')
+    plt.ylim([-0.5, 1.0])
+    plt.legend(['original lift', 'predicted lift', 'original drag', 'predicted drag'])
+    plt.savefig(os.path.join(vald_path, 'forces.jpg'))
+
+####################################### L2 error ##############################################################################
+    print("plot fields L2 error ")
+    err = field_visual_p - field_visual_t
+    err_L2 = np.linalg.norm(err, axis=(1, 2))/np.linalg.norm(field_visual_t[:, :, :, (0, 1, 1)], axis=(1, 2))
+    plt.figure(10, figsize=(15, 10))
+    plt.clf()
+    for i in range(3):
+        plt.plot(ori_input[:, -1], err_L2[:, i], '-', linewidth=2.0)
+    plt.legend(['p', 'u', 'v'])
+    plt.xlabel('Time t/s')
+    plt.ylabel('Relative $L_2$ error')
+    plt.grid()
+    plt.savefig(os.path.join(vald_path, 'L2.jpg'))
+
+####################################### plot fields gif #################################################################################
+    # plot continous fields
+    print("plot several true and predicted fields gif")
+    fig = plt.figure(100, figsize=(15, 10))
+    Visual.plot_fields_am(np.concatenate([field_visual_t, field_visual_t[(0,),]], axis=0),
+                          np.concatenate([field_visual_p, field_visual_p[(0,),]], axis=0),
+                          np.concatenate([input_visual_p, input_visual_p[(0,),]], axis=0)[:, :, :, :2],
+                          0, fig, cmin_max=[[-4, -4], [10, 4]],)
